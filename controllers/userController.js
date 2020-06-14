27200ffcb00abda8b416c65c16ca96e57621cdbe
Davy <davyfungaloi@gmail.com>
2020-06-14 15:19:58 +0200

    added bcrpyt, added front-end error display, added password validation comparison, next thing is passportjs and bcrypt comparison

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,46 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bycrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const User = require("../models/user");
 const validator = require('express-validator');
 const { body } = require('express-validator');
-
-//const { check, validationResult } = require("express-validator");
-//const express = require('express');
-//const expressValidator = ("express-validator");
-
-//const { body,validationResult } = require('express-validator/check');
-//const { sanitizeBody } = require('express-validator/filter');
-
 
 exports.index = function (req, res) {
   res.render("index", { title: "Members-Only" });
 };
 
 exports.signup_form = function (req, res) {
-  res.render("sign-up", { title: "Members-Only" });
+  res.render("sign-up", { title: "Members-Only", errors: ""});
 };
 
+
+/*
+app.post("/sign-up", (req, res, next) => {
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    const user = new User({
+      username: req.body.username,
+      password: hash,
+    }).save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
+});
+
+*/
+
 exports.signup_form_create= [
-    validator.check('password', "must be atleast 5 characters").isLength({ min: 5 }).escape(),
+    validator.check('password', "Password must be atleast 5 characters").isLength({ min: 5 }).escape(),
     validator.body('first_name', 'First Name is required').trim().isLength({min:1}).escape(),
     validator.body('last_name', 'Last Name is required').trim().isLength({min:1}).escape(),
     validator.check('username', "must be an e-mail").isEmail().escape(),
+    validator.check('password').exists(),
+    validator.check('passwordConfirmation', 'Your Password has to be the same in both fields')
+      .exists()
+      .custom((value, { req }) => value === req.body.password),
     
     //After validation & sanitzation
     (req,res, next) => {
@@ -33,16 +48,19 @@ exports.signup_form_create= [
         const errors=validator.validationResult(req)
 
         //create user with escaped & trimmed data
+
+        //bcrypt it
+        bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
         const user = new User({
             first_name: req.body.first_name,
             last_name: req.body.last_name,
             username: req.body.email,
-            passport: req.body.password,
+            passport: hash,
           });
 
           if (!errors.isEmpty()) {
               console.log(errors)
-              res.redirect("/members/signup")
+              res.render('sign-up', { title: 'Members Genre', errors: errors.array()});
               return
           }
           else {
@@ -57,79 +75,6 @@ exports.signup_form_create= [
 
           }
 
-
+        })
     }
 ]
-/*
-exports.signup_form_create = function (req, res, next) {
-  body('first_name', 'Empty name').isLength({ min: 1 }), 
-  const errors = validationResult(req);
- if(errors) {   console.log(errors)}
-  
-   
-  const user = new User({
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    username: req.body.email,
-    passport: req.body.password,
-  });
-  console.log(user);
-  user.save(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/members/signup");
-  });
-};
-
-/*
-    check('username').isEmail(),
-    check('password').isLength({ min: 5 }),
- 
-    function (req, res, next) {
-    const user = new User({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        username: req.body.email,
-        passport: req.body.password,
-      });
-        console.log(user)
-        user.save(function (err) {
-        if (err) {
-          return next(err);
-        }
-        res.redirect("/members/signup");
-      });
-      
-      console.log(req.body);
-  }
-  */
-
-/*
-exports.signup_form_create = [
-    
-    check('username').isEmail(),
-    check('password').isLength({ min: 5 }),
- 
-    function (req, res, next) {
-    const user = new User({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        username: req.body.email,
-        passport: req.body.password,
-      });
-        console.log(user)
-        user.save(function (err) {
-        if (err) {
-          return next(err);
-        }
-        res.redirect("/members/signup");
-      });
-      
-      console.log(req.body);
-  }
-
-
-
-]
-*/
